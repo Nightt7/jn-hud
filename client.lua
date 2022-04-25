91e16668ab8a588bd79f980bf9803d985a5128e1
Night <101990128+Nightt7@users.noreply.github.com>
2022-04-25 20:22:30 +0200

    Add files via upload

--- client.lua
+++ client.lua
@@ -12,22 +12,24 @@ function Status()
     self.init = function()
         CreateThread(function()
             while(true)do
-                Wait(1000)
+                Wait(300)
 
                 self.triggerEvent('esx_status:getStatus', 'hunger', function(status) 
-                    hunger = status.val / 10000 
+                    hunger = math.ceil(status.val / 10000)
                 end)
 
                 self.triggerEvent('esx_status:getStatus', 'thirst', function(status) 
-                    thirst = status.val / 10000 
+                    thirst = math.ceil(status.val / 10000)
                 end)
 
                 self.sendNui({
                     type = 'interface:update',
-                    health = math.ceil(GetEntityHealth(PlayerPedId()) / 2),
+                    health = math.ceil(GetEntityHealth(PlayerPedId()) - 100),
                     shield = GetPedArmour(PlayerPedId()),
                     hunger = hunger,
                     thirst = thirst,
+                    method = 'whenUse',
+                    percent= 50,
                 })
 
                 if(IsPedInAnyVehicle(PlayerPedId()))then
@@ -42,14 +44,10 @@ function Status()
     return self
 end
 
-RegisterNetEvent('onResourceStart', function()
+CreateThread(function()
     Status = Status()
 
-    Status.init()
-end)
-
-RegisterNetEvent('playerLoaded', function()
-    Status = Status()
-
-    Status.init()
+    SetTimeout(500, function()
+        Status.init()
+    end)
 end)
--- ui/script.js
+++ ui/script.js
@@ -1,11 +1,53 @@
 $(() => {
-    window.addEventListener('message', (event) => {
-        const e = event.data
+    const updateHudElements = (data) => {
+        let e = data
+
         if (e.type === 'interface:update'){
             $('#contvida').text(Math.round(e.health) + '%')
             $('#contshield').text(Math.round(e.shield) + '%')
             $('#contfood').text(Math.round(e.hunger) + '%')
-            $('#contwater').text(Math.round(e.thirst) + '%')
+            $('#contwater').text(Math.round(e.thirst) + '%')   
+            if(e.health === -100) {$('#contvida').text('0' + '%')}
         }
-    })
+    }
+
+    const setHudToScreen = (data, interface) => {
+        let e = data
+        if(interface.type === 'whenUse'){
+            const health = Math.round(e.health)
+            const shield = Math.round(e.shield)
+            const hunger = Math.round(e.hunger)
+            const thirst = Math.round(e.thirst)
+
+            if(health > data.percent) { $('.vida').hide(300) } else if(health < data.percent) { $('.vida').show(300) }
+            if(shield === 0) { $('.shield').hide(300) } else if(shield =~ 0) { $('.shield').show(300) }
+            if(hunger > data.percent) { $('.comida').hide(300) } else if(hunger < data.percent) { $('.comida').show(300) }
+            if(thirst > data.percent) { $('.bebida').hide(300) } else if(thirst < data.percent) { $('.bebida').show(300) }
+
+            
+        } else if(interface.type === 'default'){
+            $('.vida').show(0)
+            $('.shield').show(0)
+            $('.comida').show(0)
+            $('.bebida').show(0)
+        }
+    }
+
+    eventListener = () => {
+        self = {}
+
+        self.init = () => {
+            window.addEventListener('message', (event) => {
+                const e = event.data
+                updateHudElements(e)
+                setHudToScreen(e, {type : e.method})
+            })
+        }
+
+        return self
+    }
+
+    eventListener = eventListener()
+
+    eventListener.init()
 })
